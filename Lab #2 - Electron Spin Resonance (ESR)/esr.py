da7a645ca18956498771fd364d51945cb792d8c0
DESKTOP-1NVDVDH\my <yotamgranov@gmail.com>
2023-05-06 13:51:09 +0300

    Finished ESR lab

--- Lab #2 - Electron Spin Resonance (ESR)/esr.py
+++ Lab #2 - Electron Spin Resonance (ESR)/esr.py
@@ -10,17 +10,19 @@ print("\nPart A.1: ESR Absorption Signal Calibration...")
 Oscillator_Feedback = 44 #[%]
 Small_Coil_Freq = 96.25 + ((95.4-96.25)/(40-0))*(Oscillator_Feedback-0) #[MHz]
 
-ni_RF = Small_Coil_Freq * 10**6 #[Hz]
+nu_RF = Small_Coil_Freq * 10**6 #[Hz]
 h = scipy.constants.h #[J*sec], Planck's constant
 g = 2.0036 # [dimensionless], for electrons in DPPH
 mu_B = scipy.constants.physical_constants['Bohr magneton'][0] #[J/T], Bohr magneton
+B_res = (h*nu_RF) / (g*mu_B) #[T]
 
-B_res = (h*ni_RF) / (g*mu_B) #[T]
-#B_res_Gauss = B_res*10**4 #[G]
-#print(B_res_Gauss)
+print(f"\tSmall Coil Resonance Frequency (nu_RF): {Small_Coil_Freq:0.3f}[MHz], Resonant Magnetic Field (B_res): {B_res*10**3:0.3f}[mT]")
 
+#% Error Analysis
+del_g = 0.0002
+del_nu = 0.1 #[MHz]
 
-print(f"\tSmall Coil Resonance Frequency: {Small_Coil_Freq:0.3f}[MHz], Resonant Magnetic Field: {B_res*10**3:0.3f}[mT]")
+del_B = B_res*10**3 * np.sqrt((del_nu/Small_Coil_Freq)**2 + (del_g/g)**2) #[mT]
 
 #%% Part A.2: Measuring k under Minimal Modulation for Resonance
 print("\nPart A.2: Measuring k under Minimal Modulation for Resonance...")
@@ -28,56 +30,63 @@ print("\nPart A.2: Measuring k under Minimal Modulation for Resonance...")
 Min_Modulation = 6 # arbitrary units
 
 esra1 = pd.read_csv(f'Data/first_plot.csv', header=1, usecols=['Time (s)', '1 (VOLT)', '2 (VOLT)']) # read the data
-
 t1 = np.array(esra1['Time (s)']) # time array
 V1_1 = np.array(esra1['1 (VOLT)']) # accelerating voltage array
 V2_1 = np.array(esra1['2 (VOLT)']) # current array
 
 mu_0 = scipy.constants.mu_0 #[N/A^2], vacuum permeability
-#H = B_res # / mu_0
-H = B_res*10**3 #[mT]
+H = B_res / mu_0 #[A/m]
 
 V = 0.5*(max(V1_1) - min(V1_1)) #[V]
 R = 0.82 #[Ohm]
 I = V/R #[A]
-print(I)
+print(f"\tMagnetic Field Strength (H): {H:0.3f}[A/m], External Coil Current (I_coil): {I:0.3f}[A]")
 
-k1 = H/I #[mT/A]
-
-print(f"\tk1: {k1:0.3f}[mT/A]")
+k1 = H/I #[1/m]
+print(f"\tCoil Constant (k): {k1:0.3f}[1/m]")
 
 plt.figure(0)
-plt.plot(t1, V1_1, label='Voltage Measurement')
-plt.plot(t1, V2_1, label='Current Measurement')
-plt.xlabel("Time [sec]")
+plt.plot(t1, V1_1, label='External Coil Voltage')
+plt.plot(t1, V2_1, label='Oscillator Signal')
+plt.xlabel("Relative Time [sec]")
 plt.ylabel("Voltage [V]")
-plt.legend()
+plt.title("Signal Measurements for Minimal Modulation Resonance")
+plt.legend(loc='lower right')
 plt.grid()
 plt.show()
-
-#Oscillator_Feedback = 44 #[%]
-#Small_Coil_Freq = 96.25 + ((95.4-96.25)/(40-0))*(44-0) #[MHz]
-#print(f"\tSmall Coil Resonance Frequency: {Small_Coil_Freq:0.2f}[MHz]")
 
 #%% Part A.3: Measuring k with Direct Current
 print("\nPart A.3: Measuring k with Direct Current...")
 
 esra3 = pd.read_csv(f'Data/third_plot.csv', header=1, usecols=['Time (s)', '1 (VOLT)', '2 (VOLT)']) # read the data
 
-I_0 = 0.0933 #[A]
-k2 = H/I_0 #[mT/A]
+t3 = np.array(esra3['Time (s)']) # time array
+V1_3 = np.array(esra3['1 (VOLT)']) # accelerating voltage array
+V2_3 = np.array(esra3['2 (VOLT)']) # current array
 
-print(k2)
+V_0 = 0.390 #[V]
+I_0 = V_0 / R #[A]
+print(f"\tDC Current (I_0): {I_0:0.3f}[A]")
 
-t2 = np.array(esra3['Time (s)']) # time array
-V1_2 = np.array(esra3['1 (VOLT)']) # accelerating voltage array
-V2_2 = np.array(esra3['2 (VOLT)']) # current array
+peaks = []
+for v in range(len(V1_3)):
+    if abs(V1_3[v]-V_0) < 1*10**-2:
+        peaks.append(t3[v])
+
+k2 = H/I_0 #[1/m]
+print(f"\tCoil Constant (k): {k2:0.3f}[1/m]")
 
 plt.figure(1)
-plt.plot(t2, V1_2, label='Voltage Measurement')
-plt.plot(t2, V2_2, label='Current Measurement')
+plt.plot(t3, V1_3, label='External Coil Voltage')
+plt.plot(t3, V2_3, label='Oscillator Signal')
+for p in peaks: 
+    plt.axvline(x = p, color = 'grey', linestyle = '--')
+plt.axvline(x = -0.019, color = 'grey', linestyle = '--', label='Peaks')
+plt.axvline(x = -0.00913, color = 'grey', linestyle = '--')
+plt.axhline(y = 0.39, color = 'black', linestyle = '--', label='$V_0$')
 plt.xlabel("Time [sec]")
 plt.ylabel("Voltage [V]")
+plt.title("Signal Measurements for Direct Current Measurement")
 plt.legend()
 plt.grid()
 plt.show()
@@ -85,37 +94,93 @@ plt.show()
 #%% Part A.4: Resonance Detection using the XY Method
 print("\nPart A.4: Resonance Detection using the XY Method...")
 
-#esra2 = pd.read_csv(f'Data/second_plot.csv', header=1, usecols=['Time (s)', '1 (VOLT)', '2 (VOLT)']) # read the data
+esra2 = pd.read_csv(f'Data/second_plot.csv', header=1, usecols=['Time (s)', '1 (VOLT)', '2 (VOLT)']) # read the data
 
-#t2 = np.array(esra2['Time (s)']) # time array
-#V1_2 = np.array(esra2['1 (VOLT)']) # accelerating voltage array
-#V2_2 = np.array(esra2['2 (VOLT)']) # current array
+t2 = np.array(esra2['Time (s)']) # time array
+V1_2 = np.array(esra2['1 (VOLT)']) # accelerating voltage array
+V2_2 = np.array(esra2['2 (VOLT)']) # current array
+
+V_XY = 0.388 #[V]
+I_XY = V_XY / R #[A]
+print(f"\tExternal Coil Current (I_coil): {I_XY:0.3f}[A]")
+k3 = H/I_XY #[1/m]
+print(f"\tCoil Constant (k): {k3:0.3f}[1/m]")
+
+peaks = [-0.022135,-0.0021875,0.01781]
+for v in range(len(V1_2)):
+    if abs(V1_2[v]-V_XY) < 0.2*10**-2 or abs(V1_2[v]-(-V_XY)) < 0.2*10**-2:
+        peaks.append(t2[v])
+
+plt.figure(2)
+plt.plot(V1_2, V2_2, label='Signal')    
+plt.xlabel("External Coil Voltage [V]")
+plt.ylabel("Oscillator Signal [V]")
+plt.axvline(x = V_XY, color = 'black', linestyle = '--', label='Resonance')
+plt.axvline(x = -V_XY, color = 'black', linestyle = '--')
+plt.title("XY Signal Representation")
+plt.grid()
+plt.legend()
+
+plt.figure(3)
+plt.plot(t2, V1_2, label='External Coil Voltage', linewidth=2)
+plt.axhline(y = V_XY, color = 'black', linestyle = '--', label='Resonance')
+plt.axhline(y = -V_XY, color = 'black', linestyle = '--')
+plt.axvline(x = peaks[0], color = 'grey', linestyle = '--', label='Peaks')
+for p in peaks[1:]:
+    plt.axvline(x = p, color = 'grey', linestyle = '--')
+plt.plot(t2, V2_2, label='Oscillator Signal', linewidth=2)    
+plt.xlabel("Time [sec]")
+plt.ylabel("Voltage [V]")
+plt.title("Signal Measurements for XY Method Measurement")
+plt.legend()
+plt.grid()
+plt.show()
 
 #%% Part B.1: Measuring the Derivative of the Absorption Signal
 print("\nPart B.1: Measuring the Derivative of the Absorption Signal...")
 
+wave_gen_amplitude = 1.22 #[V] peak-to-peak
+I_start = 0.380 #[A]
+I_end = 0.540 #[A]
+
 esrb = pd.read_csv(f'Data/Part B Data.csv', header=0, usecols=['Avg(X) = I_0', 'Amp(Y)/Amp(X)']) # read the data
 I_dc = esrb['Avg(X) = I_0'] #[mA]
-derivative = esrb['Amp(Y)/Amp(X)']
+I = list(I_dc[1:]/1000) #[A]
 
-plt.figure(2)
-plt.plot(I_dc/1000, derivative)
+P_tag = esrb['Amp(Y)/Amp(X)']
+P = scipy.integrate.cumtrapz(P_tag, x=None, dx=1.0, axis=-1, initial=None)
+
+k = np.mean(k1+k2+k3)
+omega = list((2*np.pi*g*mu_B*mu_0*k/h)*(I_dc/1000)*10**-9) #[Grad/s]
+
+idx_0 = np.argmax(P)
+omega_0 = omega[idx_0+1] #[Grad/s]
+print(f"\tFree Precession Frequency (w_0): {omega_0:0.3f}[Grad/s]")
+
+T_2 = 2*np.pi / (omega[idx_0+1]) #[ns]
+print(f"\tSpin-Spin Relaxation Time (T_2): {T_2:0.3f}[ns]")
+
+plt.figure(4)
+plt.plot(I_dc/1000, P_tag)
 plt.xlabel("DC Current [A]")
 plt.ylabel("Derivative of the Absorption Signal")
 plt.title("Derivative of the Absorption Signal vs. DC Current")
 plt.grid()
-plt.show()
 
-wave_gen_amplitude = 1.22 #[V] peak-to-peak
-
-I_start = 0.380 #[A]
-I_end = 0.540 #[A]
-
-absorption = scipy.integrate.cumtrapz(derivative, x=None, dx=1.0, axis=-1, initial=None)
-plt.figure(3)
-plt.plot(I_dc[1:]/1000, absorption)
+plt.figure(5)
+plt.plot(I, P)
 plt.xlabel("DC Current [A]")
 plt.ylabel("Absorption Signal")
 plt.title("Absorption Signal vs. DC Current")
 plt.grid()
+
+plt.figure(6)
+plt.plot(omega[1:], P, label='Signal')
+plt.axvline(x = omega_0, color = 'grey', linestyle = '--', label='Resonance')
+plt.plot(omega_0, P[idx_0], '.', label='Peak')
+plt.xlabel("$\omega$ [Grad/s]")
+plt.ylabel("Absorption Signal")
+plt.title("Absorption Signal vs. Free Precession Frequency")
+plt.grid()
+plt.legend()
 plt.show()
--- Lab #3 - Hall Effect/hall.py
+++ Lab #3 - Hall Effect/hall.py
@@ -5,29 +5,68 @@ import matplotlib.pyplot as plt
 import scipy.constants
 from scipy.stats import linregress
 
-#%% Part 0: 
-print("\nPart 0: ...")
+#%% Part 0: Measure I-V (current-voltage) characteristic of the sample.
+print("\nPart 0: Measure I-V (current-voltage) characteristic of the sample....")
 
-"""
-esra1 = pd.read_csv(f'Data/first_plot.csv', header=1, usecols=['Time (s)', '1 (VOLT)', '2 (VOLT)']) # read the data
-t1 = np.array(esra1['Time (s)']) # time array
-V1_1 = np.array(esra1['1 (VOLT)']) # accelerating voltage array
-V2_1 = np.array(esra1['2 (VOLT)']) # current array
-"""
+part0data = pd.read_excel('Data/HallData.xlsx', sheet_name='Part 0')
+I_p = part0data['I_p [mA]'] #[mA]
+U_p = part0data['U_p [V]'] #[V]
 
-xData = None
-yData = None
-
-res = linregress(xData, yData) # use linear regression
-print("The slope is {:.3f} \nThe intercept value is {:.3f}".format(res.slope,res.intercept) ) # print the results of the regression
+res = linregress(I_p, U_p) # use linear regression
+R_0 = res.slope*10**3 #[Ω]
+print("The resistance of the semiconductor is {:.3f}[Ω]".format(R_0) ) # print the results of the regression
 
 plt.figure(0)
-plt.plot(xData, yData, label='')
-plt.xlabel("[]]")
-plt.ylabel("[]")
-plt.legend()
+plt.plot(I_p, U_p)
+plt.xlabel("$I_p$ [mA]")
+plt.ylabel("$U_p$ [V]")
+plt.title("Initial I-V Curve")
+plt.grid()
+#plt.show()
+
+#%% Part 1: Measure Hall voltage vs. control current
+print("\nPart 1: Measure Hall voltage vs. control current...")
+
+# Semiconductor Type: P-Type Germanium
+B = -251 #[mT]
+d = 1.00 #[mm]
+
+part1data = pd.read_excel('Data/HallData.xlsx', sheet_name='Part 1')
+I_p = pd.to_numeric(part1data['I_p [mA]'][:11]) #[mA]
+U_H = part1data['U_H [mV]'][:11] #[mV]
+
+res = linregress(I_p, U_H) # use linear regression
+R = res.slope #[Ω]
+R_H = (d*R)/(B*10**-3) #[Ω·mm/T]
+print("\tThe Hall coefficient of the semiconductor is {:.3f}[Ω·mm/T]".format(R_H))
+
+if np.sign(R_H) > 0:
+    print("\tHoles are the dominant charge carriers!")
+else:
+    print("\tElectrons are the dominant charge carriers!")
+# R_H = 1/nq
+# Since the density n must be positive, and we got R_H > 0, we see that q > 0 --> holes are dominant!
+
+q = scipy.constants.e #[C]
+n = 1 / ((R_H/1000)*q) #[m^-3]
+print("\tThe density of the majority charge carriers is {:.3f}e21[m^-3]".format(n*10**-21))
+
+plt.figure(1)
+plt.plot(I_p, U_H)
+plt.xlabel("$I_p$ [mA]")
+plt.ylabel("$U_H$ [mV]")
+plt.title("Hall Voltage vs. Control Current")
 plt.grid()
 plt.show()
 
-#%% Part 1: 
-print("\nPart 1: ...")
+#%% Part 2: Measure Hall voltage vs. magnetic field
+print("\nPart 2: Measure Hall voltage vs. magnetic field...")
+
+#%% Part 3: Measure sample voltage vs. magnetic field
+print("\nPart 3: Measure sample voltage vs. magnetic field...")
+
+#%% Part 4: Measure sample voltage vs. temperature
+print("\nPart 4: Measure sample voltage vs. temperature...")
+
+#%% Part 5: Measure the Hall voltage vs. temperature
+print("\nPart 5: Measure the Hall voltage vs temperature...")
